481ac2196c95d283df322d38615e7fc682e52e30
munisf12 <munsifalimisri69@gmail.com>
2021-05-09 08:27:56 +0500

    First redux app

---
+++ 1_react-redux-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper"></div>
+      <div className="headingWrapper">
+        <h1 className="heading">WELLCOME TO FIRST REACT REDUX APP</h1>
+      </div>
+      <div className="conunterWrapper"></div>
+    </div>
+  );
+}
+export default App;
