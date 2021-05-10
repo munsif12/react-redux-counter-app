a6fbede32a5b33dbd29cdd03119debd32525c350
munisf12 <munsifalimisri69@gmail.com>
2021-05-10 18:48:07 +0500

    main app functionality inclues useDispatch,useSelector

--- 1_react-redux-app/src/App.js
+++ 1_react-redux-app/src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
-
+import { useSelector, useDispatch } from "react-redux";
+import { incAction, decAction } from "./redux/actions";
 function App() {
+  {
+    /* useSelector takes a parameter which is store and is use to get state retrned by the reducer function like if the type is add it will return the state with increment*/
+  }
+  const value = useSelector((store) => store.reducerForIncAndDec);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <div className="wrapper"></div>
@@ -8,7 +14,23 @@ function App() {
         <h1 className="heading">WELLCOME TO FIRST REACT REDUX APP</h1>
       </div>
       <div className="conunterWrapper">
-        <button>Lets goo</button>
+        <button id="dec" onClick={() => dispatch(decAction())}>
+          -
+        </button>
+        <input type="text" name="value" id="value" value={value} />
+        {/* const dispatch = useDispatch(function) 
+        useDispatch is us to call the reducer and takes an object which includes type of the or payload if you want 
+        */}
+        <button
+          id="inc"
+          onClick={() =>
+            dispatch(
+              incAction() /* u can also do it as { type: "INC_NUMBER" } */
+            )
+          }
+        >
+          +
+        </button>
       </div>
     </div>
   );
